52dd868aeff02d95b004b7a5580d314710150bae
kavya <kavyaparengal9037@gmail.com>
2023-07-03 12:02:36 +0530

    commit

--- lib/api.dart
+++ lib/api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Api {
-  final String url = "https://ce46-117-201-193-177.ngrok-free.app";
+  final String url = "https://1516-2401-4900-3152-f105-5a9-f49b-350d-92ef.ngrok-free.app";
 
   authData(data, apiUrl) async {
     var fullUrl = url + apiUrl;
